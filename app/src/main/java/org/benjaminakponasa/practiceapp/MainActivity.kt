6b04b688b5faa59a846febb84212ec71f88be68c
adogbeji <baka_1993@yahoo.co.uk>
2023-05-24 20:07:02 +0100

    Assign ID & create variable for Button

--- app/src/main/java/org/benjaminakponasa/practiceapp/MainActivity.kt
+++ app/src/main/java/org/benjaminakponasa/practiceapp/MainActivity.kt
@@ -3,12 +3,18 @@ package org.benjaminakponasa.practiceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var resultOutput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultOutput = findViewById(R.id.resultOutput)
     }
 
     fun onDigit(view: View) {
