7523fb8099131ff499f8cf471679dc417d018bbd
adogbeji <baka_1993@yahoo.co.uk>
2023-05-29 20:24:00 +0100

    Append existing Button values

--- app/src/main/java/org/benjaminakponasa/practiceapp/MainActivity.kt
+++ app/src/main/java/org/benjaminakponasa/practiceapp/MainActivity.kt
@@ -3,6 +3,7 @@ package org.benjaminakponasa.practiceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
@@ -20,6 +21,6 @@ class MainActivity : AppCompatActivity() {
     fun onDigit(view: View) {
 //        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
 
-        resultOutput?.append("1")
+        resultOutput?.append((view as Button).text)
     }
 }
